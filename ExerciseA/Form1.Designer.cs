db40248d6de062388d4a6ca2d37dc4bb9c8bddeb
mastermartin11 <43747611+mastermartin11@users.noreply.github.com>
2018-12-10 18:22:13 +0100

    ejercicio A

--- ExerciseA/Form1.Designer.cs
+++ ExerciseA/Form1.Designer.cs
@@ -28,13 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.buttonMulti = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // buttonMulti
+            // 
+            this.buttonMulti.Location = new System.Drawing.Point(309, 314);
+            this.buttonMulti.Name = "buttonMulti";
+            this.buttonMulti.Size = new System.Drawing.Size(100, 51);
+            this.buttonMulti.TabIndex = 0;
+            this.buttonMulti.Text = "multiply";
+            this.buttonMulti.UseVisualStyleBackColor = true;
+            this.buttonMulti.Click += new System.EventHandler(this.buttonMulti_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(91, 95);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 84);
+            this.listBox1.TabIndex = 1;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(475, 95);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(120, 84);
+            this.listBox2.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.buttonMulti);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonMulti;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
     }
 }
 
--- ExerciseA/Form1.cs
+++ ExerciseA/Form1.cs
@@ -12,9 +12,40 @@ namespace ExerciseA
 {
     public partial class Form1 : Form
     {
+        List<int> numberList1 = Enumerable.Range(1, 10).ToList();
+        List<int> numberList2 = Enumerable.Range(1, 10).ToList();
+        
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void buttonMulti_Click(object sender, EventArgs e)
+        {
+            List<int> multiList = new List<int>();
+            for (int i = 0; i < 10; i++)
+            {
+                multiList.Add (numberList1[i] * numberList1[i]);
+            }
+
+             StringBuilder sb = new StringBuilder();
+            foreach (int num in multiList)
+            {
+                sb.AppendLine(num.ToString());
+            }
+            MessageBox.Show(sb.ToString());
+
+
+           
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listBox1.DataSource = numberList1;
+            listBox2.DataSource = numberList2;
+
         }
     }
 }
