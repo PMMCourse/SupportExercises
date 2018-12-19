f417aed2a5805656912e40ce00f43bcfafe38a2e
jose antonio camacho martinez <joseantonio.camacho.martinez@alumnos.fesac.es>
2018-12-19 18:18:53 +0100

    Ejercicio Terminado

--- ExerciseJ/Form1.Designer.cs
+++ ExerciseJ/Form1.Designer.cs
@@ -28,13 +28,138 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDni = new System.Windows.Forms.TextBox();
+            this.txt_dni = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtnombre = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtEdad = new System.Windows.Forms.TextBox();
+            this.btGuardar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(58, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(272, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Registro de personas";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtDni
+            // 
+            this.txtDni.Location = new System.Drawing.Point(83, 106);
+            this.txtDni.Name = "txtDni";
+            this.txtDni.Size = new System.Drawing.Size(100, 20);
+            this.txtDni.TabIndex = 1;
+            // 
+            // txt_dni
+            // 
+            this.txt_dni.AutoSize = true;
+            this.txt_dni.Location = new System.Drawing.Point(189, 109);
+            this.txt_dni.Name = "txt_dni";
+            this.txt_dni.Size = new System.Drawing.Size(32, 13);
+            this.txt_dni.TabIndex = 2;
+            this.txt_dni.Text = ": DNI";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(189, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = ": Nombre";
+            // 
+            // txtnombre
+            // 
+            this.txtnombre.Location = new System.Drawing.Point(83, 132);
+            this.txtnombre.Name = "txtnombre";
+            this.txtnombre.Size = new System.Drawing.Size(100, 20);
+            this.txtnombre.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(189, 164);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = ": Apellido";
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(83, 161);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(100, 20);
+            this.txtApellido.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(189, 190);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = ": Edad";
+            // 
+            // txtEdad
+            // 
+            this.txtEdad.Location = new System.Drawing.Point(83, 187);
+            this.txtEdad.Name = "txtEdad";
+            this.txtEdad.Size = new System.Drawing.Size(100, 20);
+            this.txtEdad.TabIndex = 7;
+            // 
+            // btGuardar
+            // 
+            this.btGuardar.Location = new System.Drawing.Point(96, 228);
+            this.btGuardar.Name = "btGuardar";
+            this.btGuardar.Size = new System.Drawing.Size(75, 23);
+            this.btGuardar.TabIndex = 9;
+            this.btGuardar.Text = "Guardar";
+            this.btGuardar.UseVisualStyleBackColor = true;
+            this.btGuardar.Click += new System.EventHandler(this.btGuardar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(363, 290);
+            this.Controls.Add(this.btGuardar);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtEdad);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtnombre);
+            this.Controls.Add(this.txt_dni);
+            this.Controls.Add(this.txtDni);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDni;
+        private System.Windows.Forms.Label txt_dni;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtnombre;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtEdad;
+        private System.Windows.Forms.Button btGuardar;
     }
 }
 
--- ExerciseJ/Form1.cs
+++ ExerciseJ/Form1.cs
@@ -16,5 +16,62 @@ namespace ExerciseJ
         {
             InitializeComponent();
         }
+        public List<Persona> ListaPersonas = new List<Persona>();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        public class Persona {
+
+            public String dni { get; set; }
+            public String nombre { get; set; }
+            public String apellido { get; set; }
+            public int edad { get; set; }
+
+            public Persona(string dni, string nombre, string apellido, int edad)
+            {
+                this.dni = dni ?? throw new ArgumentNullException(nameof(dni));
+                this.nombre = nombre ?? throw new ArgumentNullException(nameof(nombre));
+                this.apellido = apellido ?? throw new ArgumentNullException(nameof(apellido));
+                this.edad = edad;
+            }
+            public String getDni()
+            {
+                return this.dni;
+                
+            }
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btGuardar_Click(object sender, EventArgs e)
+        {
+            Persona persona = new Persona(txtDni.Text, txtnombre.Text, txtApellido.Text, Convert.ToInt32(txtEdad.Text));
+            bool b = false;
+            foreach (Persona p in ListaPersonas) {
+                if (txtDni.Text==p.getDni())
+                {
+                    MessageBox.Show("la persona ya existe");
+                    b = true;
+                }
+              
+
+            }
+            if (!b)
+                ListaPersonas.Add(persona);
+
+
+
+            txtDni.Text = "";
+            txtnombre.Text = "";
+            txtApellido.Text = "";
+            txtEdad.Text = "";
+
+          
+        }
     }
 }
